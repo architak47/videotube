aecb2589e0aaceea9a774cb405671584a7d28deb
architak47 <architawasthi01@gmail.com>
2024-05-30 12:46:52 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import dotenv from "dotenv";
 
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 
-connectDB();
-
+connectDB()
+.then( () => {
+    app.on("Error", (error) => {
+        console.log("Error : ", error);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch( (error) => {
+    console.error("MongoDB connection failed !!! ", error);
+    throw error;
+}) 
 
 
 
